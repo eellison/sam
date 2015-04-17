e2bbefa6ce24040abbac254658b5d65c7284d06d
youngrrrr <young-rae_kim@brown.edu>
2015-04-17 19:37:19 -0400

    made a few changes to the song-related stuff

--- src/main/resources/static/js/songs.js
+++ src/main/resources/static/js/songs.js
@@ -1,117 +1,25 @@
-var textInputA = $("#actor1");
-var suggestionListA = $("#list1");
-var buttonA = $("#button1");
-buttonA.text("Hide");
-
-var showSuggestionsA = true;
-/*
-	Bindings
-*/
-//When the client updates the text
-textInputA.on('input', function(e) {
-	$.post("/suggest", {line: textInputA.val()}, function(responseJSON) {
-		suggestionListA.empty();
-
-		var responseObject = JSON.parse(responseJSON);
-		var suggestions = responseObject.suggestions;
-		
-		for (var i = 0; i < suggestions.length; i++) {
-			var s = document.createElement("li");
-			s.appendChild(document.createTextNode(suggestions[i]));
-			s.addEventListener('click', function(e) {
-				textInputA.val($(this).text());
-				textInputA.focus();
-			});
-
-  			suggestionListA.append(s);
-		}
-
-		if (suggestions.length = 0) {
-			buttonA.hide();
-		} else {
-			buttonA.show();
-		}
-	});
+$(".form-group #directory-select").change(function() {
+	var soundFiles = findSoundFiles($( ".form-group #directory-select" )[0].files);
+	
 });
 
-buttonA.on('click', function(e) {
-	if (showSuggestionsA) {
-		buttonA.text("Show");
-		suggestionListA.hide();
-	} else {
-		buttonA.text("Hide");
-		suggestionListA.show();
-	}
+function endsWithIgnoreCase(str, suffix) {
+    return str.toLowerCase().indexOf(suffix.toLowerCase(), str.length - suffix.length) !== -1;
+}
 
-	showSuggestionsA = !showSuggestionsA;
-});
+function findSoundFiles(files) {
+	var soundFiles = [];
+	var acceptedTypes = [".mp3", ".wav"];
 
-var textInputB = $("#actor2");
-var suggestionListB = $("#list2");
-var buttonB = $("#button2");
-buttonB.text("Hide");
-
-var showSuggestions = true;
-/*
-	Bindings
-*/
-//When the client updates the text
-textInputB.on('input', function(e) {
-	$.post("/suggest", {line: textInputB.val()}, function(responseJSON) {
-		suggestionListB.empty();
-
-		var responseObject = JSON.parse(responseJSON);
-		var suggestions = responseObject.suggestions;
-		
-		for (var i = 0; i < suggestions.length; i++) {
-			var s = document.createElement("li");
-			s.appendChild(document.createTextNode(suggestions[i]));
-			s.addEventListener('click', function(e) {
-				textInputB.val($(this).text());
-				textInputB.focus();
-			});
-
-  			suggestionListB.append(s);
-		}
-
-		if (suggestions.length = 0) {
-			buttonB.hide();
-		} else {
-			buttonB.show();
-		}
-	});
-});
-
-buttonB.on('click', function(e) {
-	if (showSuggestions) {
-		buttonB.text("Show");
-		suggestionListB.hide();
-	} else {
-		buttonB.text("Hide");
-		suggestionListB.show();
-	}
-
-	showSuggestions = !showSuggestions;
-});
-
-var buttonC = $("#button3");
-
-buttonC.on('click', function(e) {
-	alert("Searching...");
-	$.post("/search", {name1: textInputA.val(), name2: textInputB.val()}, function(responseJSON) {
-		var responseObject = JSON.parse(responseJSON);
-
-		if (!responseObject.hasOwnProperty("error")) {
-			var element = document.getElementById("results");
-			while (element.firstChild) {
-    			element.removeChild(element.firstChild);
+	for (var i = 0; i < files.length; i++) {
+		for (var j = 0; j < acceptedTypes.length; j++) {
+			if (endsWithIgnoreCase(files[i], acceptedTypes[j])) {
+				soundFiles.push(files[i]);
+				break;
 			}
-
-			element.innerHTML = responseObject.html;
-		} else {
-			alert(responseObject.error);
 		}
-	});
-});
+	}
 
+	return soundFiles;
+}
 
