940b2a6b9fa3f526421eb5ad3c6c255833e1ccd7
Vanya Cohen <vanya_cohen@brown.edu>
2015-04-21 12:42:22 -0400

    gui

--- src/main/resources/static/js/server.js
+++ src/main/resources/static/js/server.js
@@ -1,16 +1,61 @@
 var clientsCanvas = $("#clients-canvas");
 
 $("#clients-canvas").click(function(event){
-	var x = event.pageX - $("#clients-canvas")[0].offsetLeft;
-	var y = event.pageY - $("#clients-canvas")[0].offsetTop;
+	var xPos = event.pageX - $("#clients-canvas")[0].offsetLeft;
+	var yPos = event.pageY - $("#clients-canvas")[0].offsetTop;
 	alert("x:" + x + " y:" + y);
-
+	draw_focus(xPos, yPos);
 	$.post("http://" + server_url + "/changeFocus", {x : xPos, y : yPos}, function(responseJSON) {
-			
+		
 	});
 });
+
+function draw_focus(x, y) {
+	// Get the canvas
+	var canvas = $("#clients-canvas")[0];
+	canvas.width = CANVAS_SIZE;
+	canvas.height = CANVAS_SIZE;
+
+	//Get 2D context for canvas drawing
+	var ctx = canvas.getContext("2d");
+	ctx.clearRect(CANVAS_SIZE, CANVAS_SIZE);
+
+	ctx.beginPath();
+	ctx.arc(client.x, client.y, 10, 0, 2 * Math.PI);
+	ctx.stroke();
+}
+
+/* Update Client Positions */
+function updateClientPositions() {
+	$.get("http://" + server_url + "/clientPositions", {width : CANVAS_SIZE, height : CANVAS_SIZE}, function(responseJSON) {
+		console.log("Updated clients");
+		var responseObject = JSON.parse(responseJSON);
+		var clients = responseObject;
+		
+		console.log(clients)
+		draw_clients(clients);
+	});
+}
+
+function draw_clients(clients) {
+	// Get the canvas
+	var canvas = $("#clients-canvas")[0];
+	canvas.width = CANVAS_SIZE;
+	canvas.height = CANVAS_SIZE;
+
+	//Get 2D context for canvas drawing
+	var ctx = canvas.getContext("2d");
+	ctx.clearRect(CANVAS_SIZE, CANVAS_SIZE);
+
+	for (client in clients) {
+		ctx.beginPath();
+		ctx.arc(client.x, client.y, 10, 0, 2 * Math.PI);
+		ctx.stroke();
+	}
+}
 
 $("#server-create").click(function(event) {
 	$.post("/startServer", {}, function(responseJSON) {});
 	alert("Started server");
+	var updateClientPositions = setInterval(updateClientPositions, 1000);
 });
