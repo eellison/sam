b845d50dad2a56a2cebd4176b1ab935085983472
eellison <elias_ellison@brown.edu>
2015-04-29 23:04:39 -0400

    old

--- src/main/resources/static/js/client.js
+++ src/main/resources/static/js/client.js
@@ -24,8 +24,20 @@ var updateSongTitleTimer;
 var updateVolumeTimer;
 var updateClientPositions;
 
-var time = new Date();
-time = Date(NTP.fixTime(time.getTime()));
+
+var ntpClient = require('ntp-client');
+ 
+ntpClient.getNetworkTime("pool.ntp.org", 123, function(err, date) {
+    if(err) {
+        console.error(err);
+        return;
+    }
+ 
+    console.log("Current time : ");
+    console.log(date); // Mon Jul 08 2013 21:31:31 GMT+0200 (Paris, Madrid (heure d’été)) 
+});
+
+
 
 $("#clients-canvas").click(function(event) {
 	if (connected) {
